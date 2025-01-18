63f0b01b7af2efa7317e6648888dd6ff990b9ab5
chariff16 <lotfibusi@gmail.com>
2025-01-18 16:58:30 +0100

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      'uploadthing.com',
+      'utfs.io',
+      'img.clerk.com',
+      'subdomain',
+      'files.stripe.com',
+    ],
+  },
+  reactStrictMode: false,
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
